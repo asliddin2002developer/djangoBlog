690d715f6c5eb32af7f3bae60fb713cf7dab6226
asliddin2002developer <eshonkulovasliddindevloper@gmail.com>
2022-06-12 18:13:09 +0500

    Herokuga tayyorlash

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-peozxct72y))@^x5b=wj#pp$!ys&kybz6qlpap1ln(ewzov82z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
